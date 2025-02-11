06a02628344e78a956d8f119fb2bd54be30c6b24
KNXCSdev <michal.miksho@gmail.com>
2025-02-11 12:41:15 +0100

    generic Types

--- src/genericType.ts
+++ src/genericType.ts
@@ -1,1 +1,73 @@
+//NOTE Generics in TypeScript allow you to create reusable and flexible components that work with multiple types instead of being restricted to a specific one.
+// NOTE They act as placeholders for a type, making your code more scalable and type-safe.
+
 const names: Array<string> = [];
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return { ...objA, ...objB };
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = `Got 1 element.`;
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+class DataStorage<T extends string | boolean | number> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return;
+
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Maxi");
+textStorage.removeItem("Maxi");
+textStorage.getItems();
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Max" });
+// objStorage.addItem({ name: "Maxi" });
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const namesssss: Readonly<string[]> = ["Max", "Anna"];
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,9 @@
+//NOTE An interface describes the structure of an object
+//NOTE We can use it to describe how an object should look like
+//NOTE It only exist in typescript, not in vanilla JavaScript
+//NOTE Unless a class we won't use it as a blueprint, just as a custom type (more or less?)
+//NOTE We can't assign values as in initialization
+
 // type AddFn = (a: number, b: number) => number;
 interface AddFn {
   (a: number, b: number): number;
