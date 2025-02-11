4d4313bed9c6dcefb030b68899919ca1b15b4bb4
KNXCSdev <michal.miksho@gmail.com>
2025-02-11 17:45:38 +0100

    decorators p.1

---
+++ src/types kinda.ts
@@ -0,0 +1,16 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+
+//TO SAY THAT THIS FUNCTION SHOULD NEVER RETURN ANYTHING
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occurred!", 500);
